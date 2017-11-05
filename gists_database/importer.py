d035f547cfee2d1a6e795e2bb9b373cd5548e743
Laura Uzcategui <laura.uzcateguij@gmail.com>
2017-11-05 22:02:10 +0000

    Including the work in the iterator

--- gists_database/importer.py
+++ gists_database/importer.py
@@ -1,5 +1,42 @@
 import requests
+import pprint
+import sqlite3
+
+BASE_URL = 'https://api.github.com/users/{}/gists'
+
+# Dorota - Add the query before the methods
+SQL_GIST_QUERY = """INSERT INTO gists (github_id, html_url, git_pull_url, git_push_url, commits_url, 
+            forks_url, public, created_at, updated_at, comments, comments_url) 
+            VALUES (:id, :html_url, :git_pull_url, :git_push_url, :commits_url, :forks_url, :public, 
+            :created_at, :updated_at,:comments, :comments_url);"""
 
 
 def import_gists_to_database(db, username, commit=True):
-    pass
+    gists = requests.get(BASE_URL.format(username))
+    gists.raise_for_status()
+    gists = gists.json()
+    
+    # Dorota - We have to loop through the gists and next save them in db. If I'm right ;)
+    # Ben - i think we can shortcut this, see below
+    # Laura - Perhaps we are missing the id :) checkout the model 
+    
+    for gist in gists:
+    #     parameters = {
+    #         "id" : gist["id"], 
+    #         "html_url" : gist["html_url"], 
+    #         "git_pull_url" : gist["git_pull_url"],
+    #         "git_push_url" : gist["git_push_url"], 
+    #         "commits_url" : gist["commits_url"], 
+    #         "forks_url" : gist["forks_url"], 
+    #         "public" : gist["public"], 
+    #         "created_at" : gist["created_at"], 
+    #         "updated_at" : gist["updated_at"], 
+    #         "comments" : gist["comments"], 
+    #         "comments_url" : gist["comments_url"]
+    #     }
+    # Ben - we can reference the gist directly as it is a dictionary, this seems to work
+        db.execute(SQL_GIST_QUERY, gist) #changed from parameters
+        
+    if commit:
+        db.commit()
+        
--- gists_database/search.py
+++ gists_database/search.py
@@ -1,5 +1,95 @@
 from .models import Gist
+import sqlite3
+
+
+class SearchExplore(object):
+    def __init__(self, db_connection):
+        self.db_connection = db_connection
+
+    def all(self):
+        return GistsIterator(self.db_connection)
+
+    def search(self, id=None, github_id=None, html_url=None, git_pull_url=None, git_push_url=None, commits_url=None, forks_url=None, public=None, created_at=None, updated_at=None, comments=None, comments_url=None):
+        params = {'id'          : None,
+                  'github_id'    : None,
+                  'html_url'     : None,
+                  'git_pull_url' : None,
+                  'git_push_url' : None,
+                  'commits_url'  : None,
+                  'forks_url'    : None,
+                  'public'       : None,
+                  'created_at'   :None,
+                  'updated_at'   : None,
+                  'comments'     : None,
+                  'comments_url' : None }
+        if not any(params.values()):
+            raise ValueError
+        return SearchGistIterator(**params)
+
+class GistsIterator(object):
+    def __init__(self,db_connection):
+        self.gists = []
+        self.idx = 0
+        self.db_connection = db_connection
+
+    def __iter__(self):
+        self.gists = []
+        self.idx = 0
+        self.select_gists()
+        return self
+
+    def select_gists(self):
+        conn = sqlite3.connect(self.db_connection)
+        cur = conn.cursor()
+        cur.execute("select * from gists")
+        for row in cur:
+            new_gist = Gist(row)
+            self.gists.append(new_gist)
+        conn.close()
+        
+    def __next__(self):
+        if self.idx + 1 > len(self.gists):
+            raise StopIteration
+        gist = self.gists[self.idx]
+        self.idx += 1
+        return gist
+
+    next = __next__        
+
+class SearchGistIterator(GistsIterator):
+    def __init__(self, db_connection, id=None, github_id=None, html_url=None, git_pull_url=None, git_push_url=None, commits_url=None, forks_url=None, public=None, created_at=None, updated_at=None, comments=None, comments_url=None):
+        filters = {  'id'           : id,
+                     'github_id'    : github_id,
+                     'html_url'     : html_url,
+                     'git_pull_url' : git_pull_url,
+                     'git_push_url' : git_push_url,
+                     'commits_url'  : commits_url,
+                     'forks_url'    : forks_url,
+                     'public'       : public,
+                     'created_at'   : created_at,
+                     'updated_at'   : updated_at,
+                     'comments'     : comments,
+                     'comments_url' : comments_url }
+
+        super(SearchGistIterator, self).__init__(db_connection)
+
+    def search_by_terms(self,gist):
+        for key, value in self.filters.items():
+            if value and getattr(gist, key) != value:
+                return False
+        return True
+    
+    def __next__(self):
+        gist = super(SearchIterator, self).__next__()
+        if self.search_by_terms(gist):
+            return gist
+        return next(self)
+    
+        next = __next__
 
 
 def search_gists(db_connection, **kwargs):
+    pass
+
+def build_query():
     pass
